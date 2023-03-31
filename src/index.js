aadee65e47b2a0bddb76582e0a6981d2a36f84c1
Danny <dprakonekham@gmail.com>
2023-03-30 23:48:03 -0700

    added background image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('test');
+import "./style.css";
+import taco from "./assets/images/background-image.jpg"
+
+function component() {
+    const content = document.createElement('div');
+    content.style.display = "grid";
+    content.style.backgroundImage = 'url('+taco+')';
+    content.style.backgroundSize = "contain";
+    content.style.backgroundRepeat = "no-repeat";
+    content.classList.add('content');
+
+    const nav = document.createElement('div');
+
+    const home = document.createElement('div');
+    home.textContent = "HOME";
+
+    const menu = document.createElement('div');
+    menu.textContent = "MENU";
+
+    const about = document.createElement('div');
+    about.textContent = "ABOUT";
+
+    nav.appendChild(home);
+    nav.appendChild(menu);
+    nav.appendChild(about);
+    content.appendChild(nav);
+
+    return content;
+  }
+  
+document.body.appendChild(component());
