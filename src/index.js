3a56718a7c2787266ddded58bfb41225817b67e8
Danny <dprakonekham@gmail.com>
2023-04-23 04:50:07 -0700

    changed home interaction

--- src/index.js
+++ src/index.js
@@ -5,73 +5,91 @@ import menuLoad from "./modules/menu";
 import aboutLoad from "./modules/about";
 
 function component() {
-    const container = document.createElement("div");
-    container.classList.add("container");
-    container.style.display = "flex";
-    container.style.flexDirection = "column";
-    container.style.backgroundImage = 'url('+taco+')';
-    container.style.backgroundSize = "cover";
-    container.style.backgroundRepeat = "no-repeat";
+  const container = document.createElement("div");
+  container.classList.add("container");
+  container.setAttribute("id","container");
+  container.style.display = "flex";
+  container.style.flexDirection = "column";
+  container.style.backgroundImage = 'url('+taco+')';
+  container.style.backgroundSize = "cover";
+  container.style.backgroundRepeat = "no-repeat";
 
-    const nav = document.createElement("div");
-    nav.style.display = "flex";
-    nav.style.alignItems = "center";
-    nav.style.justifyContent = "center";
-    nav.style.gap = "20px";
-    nav.style.height = "10vh";
+  const nav = document.createElement("div");
+  nav.style.display = "flex";
+  nav.style.alignItems = "center";
+  nav.style.justifyContent = "center";
+  nav.style.gap = "20px";
+  nav.style.height = "10vh";
 
-    const home = document.createElement("button");
-    home.textContent = "HOME";
-    home.style.background = "none";
-    home.style.color = "white";
-    home.style.border = "none";
-    home.style.fontSize = "24px";
+  const home = document.createElement("button");
+  home.textContent = "HOME";
+  home.style.background = "none";
+  home.style.color = "white";
+  home.style.border = "none";
+  home.style.fontSize = "24px";
 
-    const menu = document.createElement("button");
-    menu.textContent = "MENU";
-    menu.style.background = "none";
-    menu.style.color = "white";
-    menu.style.border = "none";
-    menu.style.fontSize = "24px";
+  const menu = document.createElement("button");
+  menu.textContent = "MENU";
+  menu.style.background = "none";
+  menu.style.color = "white";
+  menu.style.border = "none";
+  menu.style.fontSize = "24px";
 
-    const about = document.createElement("button");
-    about.textContent = "ABOUT";
-    about.style.background = "none";  
-    about.style.color = "white";
-    about.style.border = "none";
-    about.style.fontSize = "24px";
+  const about = document.createElement("button");
+  about.textContent = "ABOUT";
+  about.style.background = "none";  
+  about.style.color = "white";
+  about.style.border = "none";
+  about.style.fontSize = "24px";
 
-    const content = document.createElement("div");
-    content.classList.add("content");
+  const content = document.createElement("div");
+  content.classList.add("content");
+  content.setAttribute("id","content");
+  const viewMenuButton = document.createElement("Button");
+  viewMenuButton.style.backgroundColor = "green";
+  viewMenuButton.textContent = "View Menu";
+  viewMenuButton.addEventListener(`click`, function(e){
+    while(content.firstChild){
+      content.removeChild(content.lastChild)
+    }
+    content.appendChild(menuLoad());
+  });
+  content.appendChild(viewMenuButton);
 
-    content.appendChild(homeLoad());
-
-    home.addEventListener(`click`, function(e){
+  home.addEventListener(`click`, function(e){
+    while(content.firstChild){
+      content.removeChild(content.lastChild)
+    }
+    const viewMenuButton = document.createElement("Button");
+    viewMenuButton.style.backgroundColor = "green";
+    viewMenuButton.textContent = "View Menu";
+    viewMenuButton.addEventListener(`click`, function(e){
       while(content.firstChild){
         content.removeChild(content.lastChild)
       }
-      content.appendChild(homeLoad());
-    });
-    menu.addEventListener(`click`, function(e){
-      while(content.firstChild){
-        content.removeChild(content.lastChild)
-      }[]
       content.appendChild(menuLoad());
     });
-    about.addEventListener(`click`, function(e){
-      while(content.firstChild){
-        content.removeChild(content.lastChild)
-      }
-      content.appendChild(aboutLoad());
-    });
+    content.appendChild(viewMenuButton);
+  });
+  menu.addEventListener(`click`, function(e){
+    while(content.firstChild){
+      content.removeChild(content.lastChild)
+    }
+    content.appendChild(menuLoad());
+  });
+  about.addEventListener(`click`, function(e){
+    while(content.firstChild){
+      content.removeChild(content.lastChild)
+    }
+    content.appendChild(aboutLoad());
+  });
 
-    nav.appendChild(home);
-    nav.appendChild(menu);
-    nav.appendChild(about);
-    container.appendChild(nav);
-    container.appendChild(content);
+  nav.appendChild(home);
+  nav.appendChild(menu);
+  nav.appendChild(about);
+  container.appendChild(nav);
+  container.appendChild(content);
 
-    return container;
-  }
-  
+  return container;
+}
 document.body.appendChild(component());
--- src/modules/home.js
+++
@@ -1,9 +0,0 @@
-function homeLoad(){
-    const homeContainer = document.createElement("div");
-    const viewMenuButton = document.createElement("Button");
-    viewMenuButton.textContent = "View Menu";
-    homeContainer.appendChild(viewMenuButton);
-    return homeContainer;
-}
-
-export default homeLoad;
